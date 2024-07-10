8a272481b9f52938fa31fe8023d5d691b7dab237
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-10 11:40:12 +0530

    Finish dob validation

--- bean-validation/src/main/java/DobValidator.java
+++ bean-validation/src/main/java/DobValidator.java
@@ -9,14 +9,19 @@ public class DobValidator implements ConstraintValidator<Dob, LocalDate> {
 
     @Override
     public boolean isValid(LocalDate birthday, ConstraintValidatorContext context) {
-        var elContext = context.unwrap(HibernateConstraintValidatorContext.class);
-        elContext.addExpressionVariable("abc", this);
+//        var elContext = context.unwrap(HibernateConstraintValidatorContext.class);
+//        elContext.addExpressionVariable("abc", this);
         LocalDate today = LocalDate.now();
         int years = Period.between(birthday, today).getYears();
+
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(years < 18 ? "18 ata adui": "60 ata wadi")
+                .addConstraintViolation();
+
         return years >= 18 && years <= 60;
     }
 
-    public boolean isChild(LocalDate birthday){
-        return Period.between(birthday, LocalDate.now()).getYears() < 18;
-    }
+//    public boolean isChild(LocalDate birthday){
+//        return Period.between(birthday, LocalDate.now()).getYears() < 18;
+//    }
 }
